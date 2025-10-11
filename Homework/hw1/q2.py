9361f5e4bebd0a9c61856e8b9e6b47985f1f69b4
Vishruth Raj <vishraj248@gmail.com>
2025-10-11 18:02:05 -0500

    Add name and netid to submission

--- Homework/hw1/q2.py
+++ Homework/hw1/q2.py
@@ -1,6 +1,11 @@
 ###Q2: Parameter Server###
 ###please implement PS method, using  parameter update&optimizor states all be held in rank0###
 
+"""
+Vishruth Raj
+vmraj2
+"""
+
 from torch._utils import _flatten_dense_tensors, _unflatten_dense_tensors
 import torch
 import torch.distributed as dist
--- Homework/hw1/run.py
+++ Homework/hw1/run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 """
 test_cpu_hybrid_pp.py
 
